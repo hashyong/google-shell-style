37741d344696f10debbff70cf470812649a3f7a8
hashyong <564790073@qq.com>
2016-09-29 00:26:13 +0800

    change formal

--- error.sh
+++ error.sh
@@ -3,10 +3,10 @@
 error=2
 
 err() {
-	echo "[$(date +'%Y-%m-%d T %H:%M:%S%z')]: $@" >&2
+  echo "[$(date +'%Y-%m-%d T %H:%M:%S%z')]: $@" >&2
 }
 
 if true ; then
-	err "unable to do_something"
-	exit "${error}"
+  err "unable to do_something"
+  exit "${error}"
 fi
